d763486d4f66ab3c41e9b9f77611ab235168a669
권호중 <hjkwon@wisenut.co.kr>
2025-02-17 15:46:11 +0900

    feat: 로그인 페이지 및 인증 미들웨어 추가, Dex인증 예제 반영

---
+++ server/api/auth/[...].ts
@@ -0,0 +1,35 @@
+import { NuxtAuthHandler } from '#auth'
+
+import https from 'https'
+
+export default NuxtAuthHandler({
+  providers: [
+    {
+      id: 'dex',
+      name: 'Dex',
+      type: 'oauth',
+      wellKnown: 'http://local.dashboard.kubeflow.labs.wisenut.com/dex/.well-known/openid-configuration',
+      authorization: { 
+        url: 'http://local.dashboard.kubeflow.labs.wisenut.com/dex/auth',
+        params: { 
+          scope: 'openid email profile' ,
+          redirect_uri: 'http://localhost:3000/api/auth/callback/dex'
+        } 
+      },
+      
+      profile( profile:any ) {
+        console.log('profile', profile)
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+        }
+      },
+      clientId: 'kubeflow-oidc-authservice',      
+      clientSecret: 'pUBnBOY80SnXgjibTYM9ZWNzY2xreNGQok',
+
+      // httpOptions: { agent: httpsAgent }
+    }
+  ],
+})
