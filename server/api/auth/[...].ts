49dd6fafd5a737686b6fda98c491a642700fc7e0
권호중 <hjkwon@wisenut.co.kr>
2025-02-18 14:00:03 +0900

    fix: url 설정 변경

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -10,12 +10,13 @@ export default NuxtAuthHandler({
       // wellKnown: 'https://local.dashboard.kubeflow.labs.wisenut.com/dex/.well-known/openid-configuration',
       wellKnown: 'http://dex.auth.svc.cluster.local:5556/dex/.well-known/openid-configuration',
       authorization: { 
-        // url: 'https://local.dashboard.kubeflow.labs.wisenut.com/dex/auth',
-        url: 'http://dex.auth.svc.cluster.local:5556/dex/auth',
+        url: 'https://local.dashboard.kubeflow.labs.wisenut.com/dex/auth',
+        // url: 'http://dex.auth.svc.cluster.local:5556/dex/auth',
         params: { 
           scope: 'openid email profile' ,
           // redirect_uri: 'http://localhost:3000/api/auth/callback/dex'
           redirect_uri: 'https://labs.wisenut.kr/clusters/local/namespaces/wise-mlops/services/web-v2/api/auth/callback/dex'
+          // redirect_uri: 'https://labs.wisenut.kr/clusters/local/namespaces/wise-mlops/services/web-v2/oauth2/callback',
           // redirect_uri: 'http://localhost:3000/api/auth/callback/dex'
         } 
       },
