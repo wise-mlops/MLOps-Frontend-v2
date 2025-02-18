0cc5fde72769f60b76ea23f275cf6eda3957020f
권호중 <hjkwon@wisenut.co.kr>
2025-02-18 16:28:22 +0900

    fix:redirect_uri 제거하고 테스트

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -7,19 +7,20 @@ export default NuxtAuthHandler({
       name: 'Dex',
       type: 'oauth',
       // issuer: 'http://local.dashboard.kubeflow.labs.wisenut.com/dex',
-      // wellKnown: 'https://local.dashboard.kubeflow.labs.wisenut.com/dex/.well-known/openid-configuration',
-      wellKnown: 'http://dex.auth.svc.cluster.local:5556/dex/.well-known/openid-configuration',
+      wellKnown: 'https://local.dashboard.kubeflow.labs.wisenut.com/dex/.well-known/openid-configuration',
+      // wellKnown: 'http://dex.auth.svc.cluster.local:5556/dex/.well-known/openid-configuration',
       authorization: { 
         url: 'https://local.dashboard.kubeflow.labs.wisenut.com/dex/auth',
         // url: 'http://dex.auth.svc.cluster.local:5556/dex/auth',
         params: { 
           scope: 'openid email profile' ,
           // redirect_uri: 'http://localhost:3000/api/auth/callback/dex'
-          redirect_uri: 'https://labs.wisenut.kr/clusters/local/namespaces/wise-mlops/services/web-v2/api/auth/callback/dex'
+          // redirect_uri: 'https://labs.wisenut.kr/clusters/local/namespaces/wise-mlops/services/web-v2/api/auth/callback/dex'
           // redirect_uri: 'https://labs.wisenut.kr/clusters/local/namespaces/wise-mlops/services/web-v2/oauth2/callback',
           // redirect_uri: 'http://localhost:3000/api/auth/callback/dex'
         } 
       },
+
       
       profile( profile:any ) {
         console.log('profile', profile)
