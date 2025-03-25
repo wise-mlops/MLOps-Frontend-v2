8f57e8a3bc2ab1c11a2c22f4df0ae18517298b5f
권호중 <hjkwon@wisenut.co.kr>
2025-03-25 11:04:08 +0900

    fix: info-load-data 컴포넌트 코드 정리

--- components/workflow/nodes/info-load-data.vue
+++ components/workflow/nodes/info-load-data.vue
@@ -29,6 +29,7 @@ interface Item {
   id: string
   label: string
   type: string
+  options?: string[]
   value: any
 }
 
@@ -39,6 +40,87 @@ interface ItemTemplate {
 const params = defineModel<Attribute>({ default: [] })
 // const isEditable = defineModel('isEditable', { default: false })
 const isEditable = ref(true)
+const items = ref<Item[]>()
+const componentType = ref('')
+const componentTypeValue = ref('')
+
+// items 변경 감지
+watch(items, (newItems) => {
+  // 재귀 방지를 위해 값이 실제로 변경되었는지 확인
+  if (!newItems) return;
+  const newAttribute = itemsToAttribute(newItems)
+  // if (JSON.stringify(newAttribute) !== JSON.stringify(params.value)) {
+  if (params.value['type'] === componentType.value) {
+    params.value = newAttribute
+  }
+
+}, { deep: true })
+
+// componentType 변경 감지
+watch(componentTypeValue, (newType: any) => {
+
+  if (!newType.value) {
+    return
+  }
+  componentType.value = newType.value
+  let template = itemTemplate.value[newType.value]
+  items.value = template
+
+  if (params.value['type'] == newType.value) {
+    items.value = attributeToItems(params.value)
+  } else {
+    params.value = itemsToAttribute(template)
+  }
+})
+
+// params 변경 감지
+// watch(() => params.value, (newParams) => {
+
+//   if (newParams && Object.keys(newParams).length > 0) {
+//     const newItems = attributeToItems(newParams)
+//     if (JSON.stringify(newItems) !== JSON.stringify(items.value)) {
+//       items.value = newItems
+//     }
+//   }
+// }, { deep: true })
+
+onMounted(() => {
+  // 초기값 설정
+  if (params.value['type']) {
+    componentType.value = params.value['type']
+    componentTypeValue.value = params.value['type']
+    items.value = attributeToItems(params.value)
+  }
+
+})
+
+// Attribute 객체를 items 배열로 변환하는 함수
+const attributeToItems = (attr: Attribute, existingItems: Item[] = []) => {
+
+  return Object.entries(attr).map(([key, value]) => {
+    // 기존 items에서 동일한 id를 가진 항목 찾
+    const existingItem = existingItems.find(item => item.id === key)
+    return {
+      id: key,
+      label: key.charAt(0).toUpperCase() + key.slice(1).replace(/_/g, ' '), // snake_case를 타이틀 케이스로 변환
+      // type: typeof value,
+      type: existingItem?.type || '',
+      options: existingItem?.options || [],
+      value: value
+    };
+  }) as Item[];
+};
+// items 배열을 Attribute 객체로 변환하는 함수
+const itemsToAttribute = (items: Item[]) => {
+
+  const attribute: Attribute = {}
+  items.forEach(item => {
+    attribute[item.id] = item.value;
+
+  });
+
+  return attribute as Attribute;
+};
 
 const componentTypes = ref([
   {
@@ -103,81 +185,5 @@ const itemTemplate = ref<ItemTemplate>(
 
   })
 
-const items = ref<Item[]>()
-const componentType = ref('')
-const componentTypeValue = ref('')
-
-// items 변경 감지
-watch(items, (newItems) => {
-  // 재귀 방지를 위해 값이 실제로 변경되었는지 확인
-  if (!newItems) return;
-  const newAttribute = itemsToAttribute(newItems)
-  // if (JSON.stringify(newAttribute) !== JSON.stringify(params.value)) {
-  if (params.value['type'] === componentType.value) {
-    params.value = newAttribute
-  }
-
-}, { deep: true })
-
-// componentType 변경 감지
-watch(componentTypeValue, (newType: any) => {
-
-  if (!newType.value) {
-    return
-  }
-  componentType.value = newType.value
-  let template = itemTemplate.value[newType.value]
-  items.value = template
-
-  if (params.value['type'] == newType.value) {
-    items.value = attributeToItems(params.value)
-  } else {
-    params.value = itemsToAttribute(template)
-  }
-})
-
-// params 변경 감지
-// watch(() => params.value, (newParams) => {
-
-//   if (newParams && Object.keys(newParams).length > 0) {
-//     const newItems = attributeToItems(newParams)
-//     if (JSON.stringify(newItems) !== JSON.stringify(items.value)) {
-//       items.value = newItems
-//     }
-//   }
-// }, { deep: true })
-
-onMounted(() => {
-  // 초기값 설정
-  if (params.value['type']) {
-    componentType.value = params.value['type']
-    componentTypeValue.value = params.value['type']
-    items.value = attributeToItems(params.value)
-  }
-
-})
-
-// Attribute 객체를 items 배열로 변환하는 함수
-const attributeToItems = (attr: Attribute) => {
-
-  return Object.entries(attr)
-    .map(([key, value]) => ({
-      id: key,
-      label: key.charAt(0).toUpperCase() + key.slice(1).replace(/_/g, ' '), // snake_case를 타이틀 케이스로 변환
-      type: typeof value,
-      value: value
-    })) as Item[];
-};
-
-// items 배열을 Attribute 객체로 변환하는 함수
-const itemsToAttribute = (items: Item[]) => {
-
-  const attribute: Attribute = {}
-  items.forEach(item => {
-    attribute[item.id] = item.value;
-  });
-
-  return attribute as Attribute;
-};
 
 </script>
